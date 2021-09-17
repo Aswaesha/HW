ca54f295bd66999bc0fa9761ba71ffa3d37081c6
Aleksandra <taeyeononni@gmail.com>
2021-09-17 23:02:00 +0300

    HW for lesson 3

---
+++ HW3/src/com/lesson/Main.java
@@ -0,0 +1,16 @@
+package com.lesson;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        NumFebbonachi testNumFibonachi =new NumFebbonachi();
+//        testNumFibonachi.testNumFebbonachi();
+//
+//        MaxMinElem testMaxMinElem =new  MaxMinElem();
+//        testMaxMinElem.testMaxMinElem();
+
+//        SumNambres testSumNambres =new  SumNambres();
+//        testSumNambres.testSumNambres();
+
+    }
+}
