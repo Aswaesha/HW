70a725304abb797284d453b115bd1e89d974a7fa
Aleksandra <taeyeononni@gmail.com>
2021-09-20 19:10:25 +0300

    HW for lesson 1

---
+++ HM1/src/com/lesson/Main.java
@@ -0,0 +1,57 @@
+package com.lesson;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    /*int a = 20;
+	    int b = 9;
+        double c = (double) a / b;
+        System.out.println(c);*/
+
+        /*int a = 20;
+        int b = 11;
+        if (a % b == 0){
+            System.out.println("Делится без остатка");
+        } else {
+            System.out.println("Делится с остатком");
+        }*/
+       /* int a = 20;
+        int b = 10;
+        if (a > b){
+            System.out.println("a больше b");
+        } else {
+            System.out.println("b больше a");
+        }*/
+        /*int s, t, e, d;
+        int a = 0;
+        int b = 9999;
+        int random_number = a + (int) (Math.random() * b);
+        System.out.println(random_number);
+        t= random_number/1000;
+        s= (random_number%1000)/100;
+        d= (random_number%100)/10;
+        e= (random_number%10);
+        System.out.println(t + "-тысяч " + s + "-сотен " + d + "-десятков " + e + "-единиц");*/
+
+        /*int c = 'a'+ 1;
+        System.out.println(c);*/
+
+       /* byte a = 128;
+        System.out.println(a);*/
+
+      //  float i = 10f;
+      //  System.out.println(i/0);
+
+      //  ForLoop obj= new ForLoop();
+        //int sum = obj.testForLoop();
+        //System.out.println(sum);
+
+
+       /* While obj = new While();
+        int sum = obj.testWhile();
+        System.out.println(sum);*/
+
+
+    }
+}
---
+++ HM1/src/com/lesson/While.java
@@ -0,0 +1,16 @@
+//package com.lesson;
+//
+//public class While {
+//    public void doWhile () {
+//
+//        boolean cheack = true;
+//
+//        while (cheack){
+//            System.out.println("While");
+//            i++;
+//            if ( i == 10){
+//                cheack = false;
+//            }
+//        }
+//    }
+//}
