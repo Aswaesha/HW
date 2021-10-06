0319c8198e5f03acac6dfb3f8cad0fe990754423
Aswaesha <70840578+Aswaesha@users.noreply.github.com>
2021-10-06 22:44:39 +0300

    Add files via upload

---
+++ HW7/Main.java
@@ -0,0 +1,18 @@
+package com.lesson;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Set<String> set = new HashSet<>();
+        set.add("first");
+        set.add("next");
+        set.add("last");
+
+        System.out.println(set);
+    }
+}
